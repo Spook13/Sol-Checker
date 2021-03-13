b672846a1d5277b94e07e4539bc55c2dfb7a2d94
Aly <mckalyn18@gmail.com>
2021-03-13 15:00:09 -0600

    Editing buttons
    Added event listeners, made them display proper info, and gave them active and inactive classes

--- js/main.js
+++ js/main.js
@@ -56,52 +56,75 @@ function initMap() {
         var lng = coords.lng;
         changeCity(lat, lng);
 
+        // Weatther info appears
+        // Current weather by defaultt
         document.querySelector("#weather-info").style.display = "block";
 
-       document.querySelector("#current-btn").addEventListener("click", function (){
-         
-        document.querySelector("#current-weather").style.display = "block";
-        
-        document.querySelector("#hourly-weather").style.display ="none";
-        document.querySelector("#weekly-weather").style.display ="none";
-
-       });
-
-       document.querySelector("#hourly-btn").addEventListener("click", function (){
-        console.log("clicked");
-
-        document.querySelector("#hourly-weather").style.display = "block";
-        
-        document.querySelector("#current-weather").style.display ="none";
-        document.querySelector("#weekly-weather").style.display ="none";
-
-        
-      });
-
-      document.querySelector("#weekly-btn").addEventListener("click", function (){
-        console.log("clicked");
-
-        document.querySelector("#weekly-weather").style.display = "block";
-        
-        document.querySelector("#current-weather").style.display ="none";
-        document.querySelector("#hourly-weather").style.display ="none";
-
-        
-      });
-
     });
+
+  //  Adds click event listener to current-btn
+    document.querySelector("#current-btn").addEventListener("click", function (){
+       // Displays current weather info 
+      document.querySelector("#current-weather").style.display = "block";
+      // Removes hourly andd weekly weather info
+      document.querySelector("#hourly-weather").style.display ="none";
+      document.querySelector("#weekly-weather").style.display ="none";
+      // Displays current-btn as active button
+      document.querySelector("#current-btn").classList.add("active");
+      // Displays hourly-btn and weekly-btn as inactive
+      document.querySelector("#hourly-btn").classList.remove("active");
+      document.querySelector("#weekly-btn").classList.remove("active");
+     });
+
+    //  Adds click even listener to hourly-btn
+     document.querySelector("#hourly-btn").addEventListener("click", function (){
+      console.log("clicked");
+      //  Displays hourly weather info
+      document.querySelector("#hourly-weather").style.display = "block";
+      // Removes current and weekly weather info
+      document.querySelector("#current-weather").style.display ="none";
+      document.querySelector("#weekly-weather").style.display ="none";
+      // Displays hourly-btn as active button
+      document.querySelector("#hourly-btn").classList.add("active");
+      // Displays current-btn and weekly-btn as inactive
+      document.querySelector("#current-btn").classList.remove("active");
+      document.querySelector("#weekly-btn").classList.remove("active");
+    });
+
+    //  Adds click even listener to weekly-btn
+    document.querySelector("#weekly-btn").addEventListener("click", function (){
+      console.log("clicked");
+      //  Displays weekly weather info
+      document.querySelector("#weekly-weather").style.display = "block";
+      // Removes current and hourly weather info
+      document.querySelector("#current-weather").style.display ="none";
+      document.querySelector("#hourly-weather").style.display ="none";
+      // Displays weekly-btn as active button
+      document.querySelector("#weekly-btn").classList.add("active");
+      // Displays current-btn and hourly-btn as inactive
+      document.querySelector("#current-btn").classList.remove("active");
+      document.querySelector("#hourly-btn").classList.remove("active");
+    });
+
+    // Adds click even listener to f-btn
+    document.querySelector("#f-btn"). addEventListener("click", function (){
+      console.log("F is better than C");
+      // Displays f-btn as active
+      document.querySelector("#f-btn").classList.add("active");
+      // Displays c-btn and as inactive
+      document.querySelector("#c-btn").classList.remove("active");
+    });
+    
+     // Adds click even listener to c-btn
+    document.querySelector("#c-btn"). addEventListener("click", function (){
+      console.log("C is better than F");
+      // Displays c-btn as active
+      document.querySelector("#c-btn").classList.add("active");
+      // Displays f-btn as inactive
+      document.querySelector("#f-btn").classList.remove("active");
+    });
+
+
   }
 
-  function toggleWeather () {
-    const weatherBtn = document.querySelector("#weather-btn");
-    // document.querySelector("current-btn");
-    // document.querySelector("houly-btn");
-    // document.querySelector("weekly-btn");
-
-    // document.querySelector("#current-weather");
-    // docutment.querySelector("#hourly-weather");
-    // document.querySelector("#weekly-weather");
-
-    
-  };
 
